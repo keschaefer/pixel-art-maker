5d68cdfd3e39926f9d1474abbf6f465acc834800
keschaefer <keschaefer@gmail.com>
2018-09-18 15:04:05 -0600

    final js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
 let grid = document.querySelector('.grid');
 
 function createGrid() {
@@ -21,6 +23,7 @@ for (let i = 0; i < colorsAll.length; i++) {
     colorsAll[i].addEventListener('click', function (e) {
         brush = e.target.id;
         tempColor.setAttribute('id', brush);
+        console.log(e);
     })
 }
 
@@ -34,3 +37,5 @@ for (let i = 0; i < cells.length; i++) {
         }
     })
 }
+
+})
